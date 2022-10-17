3522c8e78b8497f3c11cbc7e7bc72bca01597aa9
lilyyang <1592218619@qq.com>
2022-10-17 22:49:37 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Lily />
+</template>
+
+<script>
+import Lily from "./components/Lily.vue";
+
+export default {
+  name: "App",
+  components: {
+    Lily,
+  },
+};
+</script>
