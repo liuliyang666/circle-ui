90fae18a0549011f35c7e26de363af4c2ca4b111
lilyyang <1592218619@qq.com>
2022-10-18 22:20:46 +0800

    搭建html结构

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div>
-    导航栏 | <router-link to="/">Lily</router-link> |
-    <router-link to="/xxx">Lily2</router-link>
-  </div>
-  <hr />
   <router-view />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import { createWebHashHistory, createRouter } from "vue-router";
-import Lily from "./components/Lily.vue";
-import Lily2 from "./components/Lily2.vue";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
 
 const history = createWebHashHistory();
 const router = createRouter({
   history,
   routes: [
-    { path: "/", component: Lily },
-    { path: "/xxx", component: Lily2 },
+    { path: "/", component: Home },
+    { path: "/doc", component: Doc },
   ],
 });
 
