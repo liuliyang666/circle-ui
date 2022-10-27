452ac9881a6fc612c5b1c8a65ab588088c18755b
lilyyang <1592218619@qq.com>
2022-10-27 22:57:31 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,35 @@
 <template>
-  <button class="circle-button" :class="{ [`circle-theme-${theme}`]: theme }">
+  <button class="circle-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`circle-theme-${theme}`]: theme,
+        [`circle-size-${size}`]: size,
+        [`circle-level-${level}`]: level,
+      };
+    });
+    return { classes };
   },
 };
 </script>
@@ -19,6 +39,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .circle-button {
   box-sizing: border-box;
   height: $h;
@@ -30,9 +51,10 @@ $radius: 4px;
   white-space: nowrap;
   background: white;
   color: $color;
-  border: 1 px solid $border-color;
+  border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -46,6 +68,81 @@ $radius: 4px;
   }
   &::-moz-focus-inner {
     border: 0;
+  }
+  &.circle-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &.hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.circle-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.circle-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.circle-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.circle-theme-button {
+    &.circle-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken(white, 5%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.circle-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.circle-theme-link {
+    &.circle-level-danger {
+      color: $red;
+      &.hover,
+      &.focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.circle-theme-text {
+    &.circle-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.circle-level-danger {
+      color: $red;
+      &.hover,
+      &.focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
