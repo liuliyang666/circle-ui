a6bdecca309a9f3362b8288f3f6ccd881562f0ff
lilyyang <1592218619@qq.com>
2022-11-03 23:07:49 +0800

    使用Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="circle-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="circle-dialog-wrapper">
-      <div class="circle-dialog">
-        <header>
-          <slot name="title" />
-          <span @click="close" class="circle-dialog-close"></span>
-        </header>
-        <main>
-          <slot name="content" />
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="circle-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="circle-dialog-wrapper">
+        <div class="circle-dialog">
+          <header>
+            <slot name="title" />
+            <span @click="close" class="circle-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content" />
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 <script lang="ts">
