cfe433612484e09b3263462063c46f0ed1614285
lilyyang <1592218619@qq.com>
2022-11-03 16:32:37 +0800

    Dialog支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,18 @@
 <template>
   <template v-if="visible">
-    <div class="circle-dialog-overlay"></div>
+    <div class="circle-dialog-overlay" @click="onClickOverlay"></div>
     <div class="circle-dialog-wrapper">
       <div class="circle-dialog">
         <header>
-          标题
-          <span class="circle-dialog-close"></span>
+          标题 <span @click="close" class="circle-dialog-close"></span>
         </header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -22,14 +21,49 @@
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  components: {
+    Button,
+  },
   props: {
     visible: {
       type: Boolean,
       default: false,
     },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
+    },
   },
-  components: {
-    Button,
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      context.emit("cancel");
+      close();
+    };
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel,
+    };
   },
 };
 </script>
