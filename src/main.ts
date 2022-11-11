00568744a945b9618947abe50cd6f075d0b901e3
lilyyang <1592218619@qq.com>
2022-11-11 23:30:49 +0800

    优化markdown

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import "./index.scss";
 import { router } from "./router";
 import "./lib/svg.js";
 import "github-markdown-css";
+import Markdown from "./components/Markdown.vue";
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+app.component("Markdown", Markdown);
