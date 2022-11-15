535201e7075331a175d62274922dd3a4276c8ca4
lilyyang <1592218619@qq.com>
2022-11-15 11:10:31 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:liuliyang666/circle-ui-website.git &&
+git push -f -u origin master &&
+cd ..
+echo https://liuliyang666.github.io/circle-ui-website/index.html
