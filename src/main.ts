c9c35adeadf046bfcae36efb6cd273777dfa6f05
lilyyang <1592218619@qq.com>
2022-11-15 16:17:19 +0800

    移动svg.js

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./lib/circle.scss";
 import "./index.scss";
 import { router } from "./router";
-import "./lib/svg.js";
+import "./assets/svg.js";
 import "github-markdown-css";
 import Markdown from "./components/Markdown.vue";
 
